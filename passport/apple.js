ec7b12da33597653e305a8edde19e92e43b87b3f
Ginam <wea9677@naver.com>
2022-11-23 09:48:34 +0900

    두번째 커밋

--- passport/apple.js
+++ passport/apple.js
@@ -9,11 +9,25 @@ module.exports = () =>{
     passport.use(
         new AppleStrategy(
             {
-                clientID: "",
-                teamID: "",
-                callbackURL: "",
-                keyID: "",
-                privateKeyLocation: ""
+                clientID: "com.herokuapp.applepassport-web",
+                teamID: "3L7RW74HCJ",
+                callbackURL: "https://applepassport.herokuapp.com/apple/auth",
+                keyID: "79KCA9TG7S",
+                privateKeyLocation: "./config/AuthKey_79KCA9TG7S.p8",
+                scope : "email, name"
+            }, async (req, accessToken, refreshToken, idToken, profile, cb) => {
+                console.log('apple profile :',
+                                profile,
+                                idToken,
+                                'access',
+                                accessToken
+
+                );
+                try {
+                    
+                } catch (error) {
+                    
+                }
             }
         )
     )
